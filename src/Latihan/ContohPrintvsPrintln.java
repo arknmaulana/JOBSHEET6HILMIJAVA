039f16f3cb03fcd3d01bb933dcb6ad9fd6b84fd1
Maulana Hilmi Arkan <maulanahelmy7@gmail.com>
2018-08-02 13:33:09 +0700

    Add files via upload

---
+++ src/Latihan/ContohPrintvsPrintln.java
@@ -0,0 +1,17 @@
+package Latihan;
+
+
+/** This code is licensed for Maulana Hilmi Arkan
+ * All right reserved
+ * /**
+ *
+ * @author HP ZBOOK 15
+ */
+public class ContohPrintvsPrintln {
+    public static void main(String[] args) {
+        
+       System.out.print("Ini teks yang dicetak dengan print()");
+       System.out.println("Sedangkan ini teks yang dicetak dengan println()");
+       System.out.print("Teks dicetak dengan print() lagi");
+    }
+}
